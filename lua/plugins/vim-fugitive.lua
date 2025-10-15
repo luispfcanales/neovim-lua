24529995cf53f56b3fb2395e1703eb4e0e04a3fa
Luis Pfuño Canales <luispfcanales@gmail.com>
2025-10-15 06:43:53 -0500

    feat: fugitive with notify

--- lua/plugins/vim-fugitive.lua
+++ lua/plugins/vim-fugitive.lua
@@ -1,6 +1,17 @@
 return {
   "tpope/vim-fugitive",
-  config=function()
-    vim.keymap.set('n', '<leader>z',"<cmd>Git<CR>")
+  config = function()
+    vim.keymap.set('n', '<leader>z', "<cmd>Git<CR>")
+    
+    -- Redirigir mensajes de vim a notify
+    local original_print = print
+    print = function(...)
+      local args = {...}
+      local msg = table.concat(vim.tbl_map(tostring, args), " ")
+      if msg:match("git") or msg:match("Git") then
+        vim.notify(msg, vim.log.levels.INFO, { title = "Git" })
+      end
+      original_print(...)
+    end
   end,
 }
