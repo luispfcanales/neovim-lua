8923aa5d78a0f341234da836cdea4ba36f4cf798
Luis Pfuño Canales <luispfcanales@gmail.com>
2025-10-29 10:52:13 -0500

    feat: width harpoon set

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -4,15 +4,25 @@ return {
     "nvim-lua/plenary.nvim",
   },
   config = function()
-    require("harpoon").setup({})
+    local menu_width = 180
+    
+    if vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1 then
+      menu_width = 190
+    end
+
+    require("harpoon").setup({
+      menu = {
+        width = menu_width,
+      }
+    })
 
     local keymap = vim.keymap
     local opts = { noremap = true, silent = true }
 
-    keymap.set("n", "<A-m>", [[<cmd>lua require("harpoon.mark").add_file()<CR>]],opts)
-    keymap.set("n", "<A-a>", [[<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>]],opts)
-    keymap.set("n", "<A-1>", [[<cmd>lua require("harpoon.ui").nav_file(1)<CR>]],opts)
-    keymap.set("n", "<A-2>", [[<cmd>lua require("harpoon.ui").nav_file(2)<CR>]],opts)
-    keymap.set("n", "<A-3>", [[<cmd>lua require("harpoon.ui").nav_file(3)<CR>]],opts)
+    keymap.set("n", "<A-m>", [[<cmd>lua require("harpoon.mark").add_file()<CR>]], opts)
+    keymap.set("n", "<A-a>", [[<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>]], opts)
+    keymap.set("n", "<A-1>", [[<cmd>lua require("harpoon.ui").nav_file(1)<CR>]], opts)
+    keymap.set("n", "<A-2>", [[<cmd>lua require("harpoon.ui").nav_file(2)<CR>]], opts)
+    keymap.set("n", "<A-3>", [[<cmd>lua require("harpoon.ui").nav_file(3)<CR>]], opts)
   end,
 }
