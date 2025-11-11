13a1258ca90219bcd07d5b8fc39b8aeab1513e3b
Luis Pfuño Canales <luispfcanales@gmail.com>
2025-11-11 08:02:31 -0500

    feat: test

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -3,7 +3,7 @@ return {
   priority = 1000,
   lazy = false,
   opts = {
-    -- bigfile = { enabled = true },
+    bigfile = { enabled = true },
     -- dashboard = { enabled = true },
     -- explorer = { enabled = true },
     indent = { enabled = true },
@@ -29,7 +29,7 @@ return {
     { "<a-p>",      function() Snacks.picker.smart() end,               desc = "Smart Find Files" },
     -- { "<leader>,",       function() Snacks.picker.buffers() end,         desc = "Buffers" },
     { "<c-g>",      function() Snacks.picker.grep() end,                desc = "Grep" },
-    { "<leader>:",  function() Snacks.picker.command_history() end,     desc = "Command History" },
+    { "<a-h>",      function() Snacks.picker.command_history() end,     desc = "Command History" },
     { "<leader>n",  function() Snacks.picker.notifications() end,       desc = "Notification History" },
     { "<a-e>",      function() Snacks.explorer() end,                   desc = "File Explorer" },
     -- find
